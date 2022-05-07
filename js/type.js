095e7115c78f6f1be0628a82afb7d524029ca094
mcvuong <mcvuong@gmail.com>
2022-05-07 01:49:48 -0400

    Version 2

--- js/type.js
+++ js/type.js
@@ -1,35 +1,52 @@
-var aText = new Array(
-    "Writer. Coder.");
-    var iSpeed = 100; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.getElementById("typedtext");
-     
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-      else if (iIndex == aText.length-aText.length) {
-          setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
+//onload
+var aText = ("Content Gone Technical.").split("");
+
+var onMain = false;
+
+console.log(onMain);
+$('header').addClass("default"); 
+$('section').hide();
+typewriter("", 0);
+
+function typewriter(typed, current) //typewriter for h2
+{
+    typed += aText[current];
+    current++;
+    document.getElementById("typedtext").innerHTML = typed;
+    if (current == aText.length - 1) {
+        setTimeout(() => { typewriter(typed, current);}, 700);
     }
-    
-    
-    typewriter();
+    else setTimeout(() => {  if (current <= aText.length - 1) typewriter(typed, current);}, 50);
+}
+
+$("button").click(function() {
+    console.log("We are at mode.");
+    console.log($(this).html());
+    if ($(this).html() == "🌅") {
+        $(this).html("🎑");
+        $('#theme').attr('href','css/dark.css');
+    }
+    else {
+        $(this).html("🌅");
+        $('#theme').attr('href','css/default.css');
+    }
+})
+function toggle () {
+    console.log("We got here!");
+    onMain = !onMain;
+    console.log(onMain);
+    if (onMain == false) {
+        $('header').addClass("default");
+        $('header').removeClass("about");
+        $('section').hide();
+        typewriter("", 0);
+        console.log("We're now showing stuff");
+    }
+    else {
+        $('header').addClass("about");
+        $('header').removeClass("default");  
+        typewriter("", 0);
+
+        setTimeout(() => { $('section').fadeIn();}, 200);
+    }
+}
